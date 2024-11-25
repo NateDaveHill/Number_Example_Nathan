9bdd867abf9ad2a71967a4afbeaec29d8f1fab3e
Nathan David Hill <nathanhill@MacBook-Pro-von-Nathan.local>
2024-11-25 15:02:03 +0100

    updating code

--- db_connection.php
+++ db_connection.php
@@ -2,8 +2,7 @@
 $server = "localhost";
 $username = "root";
 $password = "";
-$dbname = "number_example_db";
-
+$dbname = "number_example_db"
 $conn = mysqli_connect($server, $username, $password, $dbname);
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
